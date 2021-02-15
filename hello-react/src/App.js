f0e68e18a8c15a2462cd7e1bfb4d4e12a256e1e9
stchoi <littlebest17@naver.com>
2021-02-15 17:51:23 +0900

    hello-react

---
+++ hello-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import MyName from './MyName';
+import MyComponents from './MyComponents';
+
+const App = () => {
+  return (
+    <MyComponents
+      name="react"
+      favoriteNumber={7}
+    >
+      리액트2
+    </MyComponents>
+  )
+};
+
+export default App;
